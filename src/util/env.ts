9f7e3ec853f986a6662eec1026392855995af200
Yoshiki Ohashi <rrrtcdish2@gmail.com>
2022-07-17 20:44:54 +0900

    fix env

--- src/util/env.ts
+++ src/util/env.ts
@@ -1,5 +1,5 @@
 export const NOTION_SECRET_API = process.env.NOTION_SECRET_API || '';
-export const NOTION_DATABASE_ID = process.env.DATABASE_ID || '';
+export const NOTION_DATABASE_ID = process.env.NOTION_DATABASE_ID || '';
 export const BLOG_DEFAULT_IMAGE = process.env.DEFAULT_IMAGE || '';
 export const UPLOADCARE_PUBLIC_KEY = process.env.UPLOADCARE_PUBLIC_KEY || '';
 export const BLOG_PATH = process.env.BLOG_PATH || '';
